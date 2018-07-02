bc8d005b16f91482fe3d39eb878105e4dc901f69
zhoujj1228 <zjj13622773976@163.com>
2018-07-02 11:38:25 +0800

    log4j example

---
+++ Log4j/src/TestLog4j.java
@@ -0,0 +1,34 @@
+import java.io.File;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.PropertyConfigurator;
+
+
+public class TestLog4j {
+	public static void main(String[] args){
+		TestLog4j tl = new TestLog4j();
+		tl.call();
+	}
+
+	private void call() {
+		String log4jPath = "/config/log4j.properties";
+		//String rootPath = this.getClass().getResource("/").getPath();
+		String rootPath = System.getProperty("user.dir");
+		log4jPath = rootPath + log4jPath;
+		/*File file = new File(log4jPath);
+		System.out.println(log4jPath + file.exists());*/
+		PropertyConfigurator.configure(log4jPath);
+		new Test().call();
+		
+	}
+}
+
+class Test{
+	private static final Log log = LogFactory.getLog(Test.class);
+
+	public void call() {
+		log.debug("test debug");
+		log.error("test error");
+	}
+}
